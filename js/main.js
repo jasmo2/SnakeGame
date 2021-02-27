9afbe54e5635ae20a3b10753020d30f4378aa509
Jaime <jasmo2.com@gmail.com>
2021-02-27 10:08:20 -0500

    restart game

--- js/main.js
+++ js/main.js
@@ -2,27 +2,49 @@ const boardBorder = 'black'
 const boardBackground = 'white'
 const snakeCol = 'lightblue'
 const snakeBorder = 'darkblue'
-
-let snake = [
+const initialSnake = [
   { x: 200, y: 200 },
   { x: 190, y: 200 },
   { x: 180, y: 200 },
   { x: 170, y: 200 },
   { x: 160, y: 200 },
+  { x: 150, y: 200 },
+  { x: 140, y: 200 },
+  { x: 130, y: 200 },
+  { x: 120, y: 200 },
 ]
+let snake = Array.from(initialSnake)
+const playAgainBtn = document.getElementById('play-again')
+
+let reverseDirection = false
+
+const SNAKE_SIZE = 10
+
 // Speed
 const SPEED = 10
-
 let dx = SPEED
 let dy = 0
 
 const snakeboard = document.getElementById('snakeboard')
 const snakeboardCtx = snakeboard.getContext('2d')
-document.addEventListener('keydown', changeDirection)
+
+playAgainBtn.onclick = restartGame
+
 // Start game
 main()
+document.addEventListener('keydown', changeDirection)
+
+function restartGame() {
+  playAgainBtn.style = 'display: none;'
+  snake = Array.from(initialSnake)
+  dx = SPEED
+  dy = 0
+  main()
+}
 
 function main() {
+  if (endGame()) return
+  reverseDirection = false
   setTimeout(() => {
     clearCanvas()
     moveSnake()
@@ -46,13 +68,53 @@ function drawSnake() {
 function drawSnakeItem(snakeItem) {
   snakeboardCtx.fillStyle = snakeCol
   snakeboardCtx.strokestyle = snakeBorder
-  snakeboardCtx.fillRect(snakeItem.x, snakeItem.y, 10, 10)
-  snakeboardCtx.strokeRect(snakeItem.x, snakeItem.y, 10, 10)
+  snakeboardCtx.fillRect(snakeItem.x, snakeItem.y, SNAKE_SIZE, SNAKE_SIZE)
+  snakeboardCtx.strokeRect(snakeItem.x, snakeItem.y, SNAKE_SIZE, SNAKE_SIZE)
+}
+
+function endGame() {
+  // Snake contact itself
+  for (let i = 4; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      playAgainBtn.style = 'display: block;'
+      return true
+    }
+  }
 }
 
 function moveSnake() {
-  const head = { x: snake[0].x + dx, y: snake[0].y }
+  const { x, y } = snake[0]
+  const head = { x: dx + x, y: dy + y }
   snake.unshift(head)
   snake.pop()
 }
+
+function changeDirection(event) {
+  const LEFT_KEY = 37
+  const RIGHT_KEY = 39
+  const UP_KEY = 38
+  const DOWN_KEY = 40
+
+  if (reverseDirection) return
+  reverseDirection = true
+
+  const keyPressed = event.keyCode
+  const goingUp = dy === -1 * SPEED
+  const goingDown = dy === SPEED
+  const goingRight = dx === SPEED
+  const goingLeft = dx === -1 * SPEED
+
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -1 * SPEED
+    dy = 0
+  } else if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0
+    dy = -1 * SPEED
+  } else if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = SPEED
+    dy = 0
+  } else if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0
+    dy = SPEED
+  }
 }
